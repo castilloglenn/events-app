a79a1f8bf1dbb929e95257b55b9452b38cbf7c98
Allen Glenn Castillo <55197203+castilloglenn@users.noreply.github.com>
2023-09-22 19:45:37 +0800

    fix: path

--- server/util.py
+++ server/util.py
@@ -2,7 +2,7 @@ import json
 
 
 def get_data() -> dict:
-    with open("server/events.json", "r") as json_data:
+    with open("events.json", "r") as json_data:
         data = json.load(json_data)
 
         return data
