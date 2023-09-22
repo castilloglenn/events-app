548021f450b10484d9e29cdd2fc0472f2532c828
Allen Glenn Castillo <55197203+castilloglenn@users.noreply.github.com>
2023-09-22 21:38:54 +0800

    feat: CRUD base functions

--- server/database.py
+++ server/database.py
@@ -1,5 +1,18 @@
+from typed import EventDetails, EventID
 from util import get_data
+
+
+def create(data: EventDetails) -> str:
+    ...
 
 
 def read() -> dict:
     return get_data()
+
+
+def update(id: EventID, data: EventDetails) -> str:
+    ...
+
+
+def delete(id: EventID) -> str:
+    ...
