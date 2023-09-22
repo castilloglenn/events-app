0465861aae05c336a8452b69d81e8182b87a6220
Allen Glenn Castillo <55197203+castilloglenn@users.noreply.github.com>
2023-09-22 21:48:01 +0800

    feat: create event

--- server/server.py
+++ server/server.py
@@ -4,6 +4,13 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 
+@app.route("/create-event", methods=["POST"])
+def create_event():
+    data = request.json
+    result = create(data)
+    return jsonify({"message": result})
+
+
 @app.route("/list-events", methods=["GET"])
 def list_events():
     events = read()
