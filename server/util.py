98b7347c2346f0072f02e4a21ebb1072cc05fc81
Allen Glenn Castillo <55197203+castilloglenn@users.noreply.github.com>
2023-09-23 12:34:15 +0800

    feat: event validator

--- server/util.py
+++ server/util.py
@@ -32,4 +32,35 @@ def save_data(data: dict):
 
 
 def validate_event(db: dict[str, dict[str, Any]], event: dict[str, Any]) -> str:
-    return "Testing testing"
+    new_event = deserialize_datetime(event)
+    new_start: datetime = new_event["start"]
+    new_end: datetime = new_event["end"]
+
+    valid_start = new_start.replace(hour=8, minute=0, second=0, microsecond=0)
+    valid_end = new_start.replace(hour=20, minute=0, second=0, microsecond=0)
+
+    now = datetime.now()
+
+    if new_start > new_end:
+        return "You cannot set the event's start time ahead of the end time."
+    elif new_start < now or new_end < now:
+        return "You cannot create an event that already past the current time."
+    elif not (valid_start <= new_start <= valid_end) or not (
+        valid_start <= new_end <= valid_end
+    ):
+        return "Events to be set are only between 8AM-8PM in the evening. No events allowed outside these hours."
+
+    for evt in db.values():
+        evt_start = evt["start"]
+        evt_end = evt["end"]
+
+        if (
+            new_start >= evt_start
+            and new_start <= evt_end
+            or new_end >= evt_start
+            and new_end <= evt_end
+        ):
+            evt_name = evt["name"]
+            return f"You are overlapping with the other event: {evt_name}"
+
+    return ""
